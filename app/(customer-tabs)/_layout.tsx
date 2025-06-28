c4cf771b693e6e096da7c66940bef8efc1bd20e6
Roberto amaral <81282986+betows@users.noreply.github.com>
2025-06-28 15:13:11 -0300

    Corrigir completamente a navbar do customer

--- app/(customer-tabs)/_layout.tsx
+++ app/(customer-tabs)/_layout.tsx
@@ -1,5 +1,5 @@
 import { Tabs } from 'expo-router';
-import { Chrome as Home, User, Calendar, Users } from 'lucide-react-native';
+import { Home, Users, Calendar, User } from 'lucide-react-native';
 
 export default function CustomerTabLayout() {
   return (
